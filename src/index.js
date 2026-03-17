9ff4b8e16a6a77961c671e6dbc6d81ac78608699
Faheem <faheemxxx456@gmail.com>
2026-03-17 18:33:55 +0530

    feat: validate missing --path flag

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ const pathValue=args[pathIndex + 1]
 const byIndex=args.indexOf("--by")
 const byValue=args[byIndex+1]
 console.log(pathValue)
+if (pathIndex==-1){
+    console.log("you forgot to add your path")
+    process.exit(1)
+}
+
+console.log(pathValue)
 console.log(byValue)
