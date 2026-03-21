82718b0a51cf128870a9367a719ece7be3535619
Faheem <faheemxxx456@gmail.com>
2026-03-21 12:59:22 +0530

    feat: add express server with organize endpoint

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express=require('express')
+const express=require('express');
+const startScanning = require('./src/scanner');
+
 
 const app=express()
 app.use(express.json())
 
-app.post('/organise',(req,res)=>{
-    
+app.post  ('/organise',async(req,res)=>{
+    const path=req.body.path;
+    const by=req.body.by;
+    try{
+    await startScanning(path)
+    res.json({success:true,message:"organised"})
+        
+
+        }catch(err){
+            res.json({success:false})
+
+        }
+  
+}) 
+
+   app.listen(3000,()=>{
+    console.log('server running on port 3000')
 })
