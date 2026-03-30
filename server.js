9180ab6f9206fd32f1f70466f7d97366d09b9bf3
Faheem <faheemxxx456@gmail.com>
2026-03-30 20:15:02 +0530

    feat: implement path whitelisting and basic security guard

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const startScanning = require('./src/scanner');
-const config=require('./src/config')
-
+const config=require('./src/config');
+const pathmodule =('path');
 
 const app=express()
 app.use(express.json())
@@ -12,10 +12,17 @@ app.use((req,res,next)=>{
 
     next()
 })
-
+   const ALLOWED_FOLDER = 'C:/Users/Thispc/Downloads'
 app.post  ('/organise',async(req,res)=>{
+    const rule={
+
+    }
     const path=req.body.path;
     const by=req.body.by;
+    const resolvedPath = pathModule.resolve(userInputPath)
+    if(!path.startsWith(ALLOWED_FOLDER)){
+        return res.status(403).json({erros:"error,only access dowloads"})
+    }
     try{
     await startScanning(path)
     res.json({success:true,message:"organised"})
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const config={
     port:process.env.PORT || 3000,
-    nodeENV:process.env.NODE_ENV || development,
+    nodeENV:process.env.NODE_ENV || 'development',
 }
 
 module.exports=config
