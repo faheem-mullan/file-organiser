d8edc1e30b0048a651634f9d2d8356cbfc2b709e
Faheem <faheemxxx456@gmail.com>
2026-04-14 15:36:17 +0530

    refactor: transition to parallel I/O pipeline using async map and Promise.all

--- src/services/scanner.js
+++ src/services/scanner.js
@@ -4,34 +4,24 @@ const movefile =require("./mover")
 const path =require("path")
 const emitter=require('../utils/logger')
 
+
 const  startScanning=async(pathValue)=>{  
     const files =await fs.promises.readdir(pathValue)
     console.log(`Found ${files.length} items.`);
-    for  (const file of files){
-     
-     
-        console.log(`Checking: ${file}`);
+    const validfiles=files.filter(file=>!file.endsWith('.js'))
+    const movetask=validfiles.map(async(file)=>{
         const fullpath=path.join(pathValue,file)
-        const stats = await fs.promises.lstat(fullpath);
+        const stats= await fs.promises.lstat(fullpath)
 
-        if(stats.isDirectory()){
-            continue
-        }
-        if(file.endsWith(".js")){
-            continue
-        } 
-        const category=identifier(file)
-       console.log(`Moving ${file} to ${category}...`);
-       try{
-        await movefile(pathValue,file,category)
+        if(stats.isDirectory())
+             return null;
+            const category=identifier(file)
+            return movefile(pathValue,file,category)
         
-           emitter.emit('filemoved',file,category)}
-           catch(err){
-            console.log("ERROR",err.message)
-           }
+    
+    })
+    await Promise.all(movetask)
 
-
-    }
 }
 
 module.exports = startScanning 
